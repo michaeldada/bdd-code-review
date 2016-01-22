f7402b9b7c4461478a1c74fa5f7f9e83b7e6fe3d
michaeldada <mail.michaeldada@gmail.com>
2016-01-22 09:28:47 -0800

    add spec for replacing modulo 5

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,11 @@
 describe('countUp', function(){
   it("will count up from 1 to the given number", function(){
-    expect(countUp(5)).to.eql([1,2,'ping',4,5]);
+    expect(countUp(2)).to.eql([1,2]);
   });
   it("will replace all numbers divisible by 3 with 'ping'", function(){
     expect(countUp(5)).to.eql([1,2,'ping',4,5]);
+  });
+  it("will replace all numbers divisible by 5 with 'pong'", function(){
+    expect(countUp(10)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong'])
   });
 });
