af8374e2beb3ad2468fb9496a6a1e88df13e669d
michaeldada <mail.michaeldada@gmail.com>
2016-01-22 09:26:10 -0800

    add functionality for replacing modulo 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 var countUp = function(countTo){
   var result = [];
     for(var i = 1; i <= countTo; i++){
+      var ping = result.indexOf(i);
+      if(i % 3 === 0) {
+
+
+        result.splice(ping, 0);
+        result.push('ping');
+
+      }else{
+
+
       result.push(i);
+      }
     }
     return result;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('countUp', function(){
   it("will count up from 1 to the given number", function(){
-    expect(countUp(5)).to.eql([1,2,3,4,5]);
+    expect(countUp(5)).to.eql([1,2,'ping',4,5]);
   });
   it("will replace all numbers divisible by 3 with 'ping'", function(){
     expect(countUp(5)).to.eql([1,2,'ping',4,5]);
