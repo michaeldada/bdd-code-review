3145385cadc1443ccd1cceff07bc61ac94cd8398
michaeldada <mail.michaeldada@gmail.com>
2016-01-22 09:46:20 -0800

    add script for replacing modulo 15

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@ var countUp = function(countTo){
   var result = [];
     for(var i = 1; i <= countTo; i++){
       var index = result.indexOf(i);
-      if(i % 3 === 0) {
+      if(i % 15 === 0) {
         result.splice(index, 0);
-        result.push('ping');
+        result.push('pingpong');
       }else if(i % 5 === 0) {
         result.splice(index, 0);
         result.push('pong');
+      }else if(i % 3 === 0) {
+      result.splice(index, 0);
+      result.push('ping');
       }else{
 
 
       result.push(i);
         }
       }
-    
+
     return result;
 };
