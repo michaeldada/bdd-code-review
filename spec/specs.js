d9fc01dd72c66913be6729f48945f1fa32258b68
michaeldada <mail.michaeldada@gmail.com>
2016-01-22 09:39:42 -0800

    add spec for replacing modulo 15

--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,7 @@ describe('countUp', function(){
   it("will replace all numbers divisible by 5 with 'pong'", function(){
     expect(countUp(10)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong'])
   });
+  it("will replace all numbers divisible by 15 with 'pingpong'", function(){
+    expect(countUp(15)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'pingpong'])
+  });
 });
